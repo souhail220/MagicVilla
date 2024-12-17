1ccce6257448e2abc74d8b4a313f5d658495e924
Souhail Gana <souhailgana943@gmail.com>
2024-12-17 21:37:24 +0100

    added api's models

---
+++ MagicVilla_Web/Models/DTO/VillaNbDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Models.DTO
+{
+    public class VillaNbDTO
+    {
+        [Required]
+        public int VillaNumber { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
---
+++ MagicVilla_Web/Models/VillaNb.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_Web.Models
+{
+    public class VillaNb
+    {
+        public int VillaNumber { get; set; }
+        public string SpecialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
