7e0a9b7f3ed916676024452b9ff91fa22e523bce
Souhail Gana <souhailgana943@gmail.com>
2024-12-21 19:28:23 +0100

    added images to project

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -1,32 +1,34 @@
+using AutoMapper;
 using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.DTO;
+using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MagicVilla_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IVillaService villaService;
+        private readonly IMapper mapper;
+        public HomeController(IVillaService villaNbService, IMapper mapper)
         {
-            _logger = logger;
+            this.villaService = villaNbService;
+            this.mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            List<VillaDTO> list = new();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            var response = await villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+            return View(list);
         }
     }
 }
