76d1415fcf984036ae22d3e39ec1716b075e2e0a
Souhail Gana <souhailgana943@gmail.com>
2024-12-23 11:48:35 +0100

    Created User Login/Register DTOs

---
+++ MagicVilla_Web/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_Web.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MagicVilla_Web/Models/DTO/LoginResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_Web.Models.DTO
+{
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
